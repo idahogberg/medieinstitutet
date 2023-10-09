82ec7a4c4f64f641eee0aa563c79fad65b980fe0
celestebloss <146746351+celestebloss@users.noreply.github.com>
2023-10-09 13:12:31 +0200

    Put breaks and created JS document (script.js)

---
+++ Grupparbete/script.js
@@ -0,0 +1,22 @@
+function clickFunction() {
+
+    let ourDiv = document.querySelector("#background-changer");
+
+    ourDiv.style.backgroundColor = "tan";
+}
+
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    //If else sats
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = timeString;
+} setInterval(updateClock, 1000); updateClock();
+// Uppdatera klockan direkt vid sidans laddning
+
+
